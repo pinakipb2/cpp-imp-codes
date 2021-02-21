870dcee70026498d73f6675edd9c77d567f232a9
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2021-02-21 18:46:27 +0530

    DP

---
+++ DP/string/shortestCommonSupersequenceLength.cpp
@@ -0,0 +1,92 @@
+//Code Written by Pinaki Bhattacharjee
+#include<bits/stdc++.h>
+using namespace std;
+
+// typedef
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+// #define
+#define LOCAL
+#define MAX 10000
+#define PI 2*acos(0.0)
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define DEBUG(x) cerr << #x << " = " << x << endl;
+
+// constants
+const int MOD = (int) 1e9 + 7;
+
+// debugger
+#ifdef LOCAL
+    #define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define trace(...) 42
+#endif
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+    cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+    const char* comma = strchr(names + 1, ',');
+    cerr.write(names, comma - names) << ": " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+
+// fast I-O
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+
+int lcs(string X, string Y, int m, int n)
+{
+    int dp[m+1][n+1];
+    for(int i=0;i<m+1;i++)
+    {
+        for(int j=0;j<n+1;j++)
+        {
+            if(i==0 || j==0)
+                dp[i][j] = 0;
+        }
+    }
+    for(int i=1;i<m+1;i++)
+    {
+        for(int j=1;j<n+1;j++)
+        {
+            if(X[i-1] == Y[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
+
+int main()
+{
+    clock_t begin = clock();
+    pinakipb2();
+    // Code begins
+    
+    // string X = "geek";
+    // string Y = "eke";
+    string X = "AGGTAB";
+    string Y = "GXTXAYB";
+    int m = X.length();
+    int n = Y.length();
+    cout<<(m + n) - lcs( X, Y, m, n);  // we take the common character only once
+    
+    // Code ends
+    // execution time
+    #ifndef ONLINE_JUDGE
+        clock_t end = clock();
+        cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif
+    return 0;
+}
